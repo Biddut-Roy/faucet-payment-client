b5c2e1aba9fbf03a680f268985a823cf7b5584af
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-03-02 23:42:03 +0600

    start auth0 authentication systemp

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router/Router.jsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Auth0Provider
+      domain="dev-igwgxo1b53ze0ele.us.auth0.com"
+      clientId="Z4CN2azdLoGosO1tbux72gx4okGxdqai"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <RouterProvider router={router} />
+    </Auth0Provider>
   </React.StrictMode>,
 )
